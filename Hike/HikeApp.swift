e73546a72b5bf9e2e892a705775f6509e7eaee70
Marty2390 <129055675+Marty2390@users.noreply.github.com>
2024-11-05 15:16:49 +0000

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Chris  Martyr on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
